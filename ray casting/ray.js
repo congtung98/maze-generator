5fab9a2de21d24ce595d5929edac8e1c9deab6a3
Hoan Vu Van <congtunggv@gmail.com>
2021-07-18 11:33:52 +0700

    feature: ray casting 2D

--- ray casting/ray.js
+++ ray casting/ray.js
@@ -1,7 +1,13 @@
 class Ray {
-    constructor(x, y) {
-        this.pos = createVector(x, y);
-        this.dir = createVector(1, 0);
+    constructor(pos, angle) {
+        this.pos = pos;
+        this.dir = p5.Vector.fromAngle(angle);
+    }
+
+    lookAt(x, y) {
+        this.dir.x = x - this.pos.x;
+        this.dir.y = y - this.pos.y;
+        this.dir.normalize();
     }
 
     show() {
@@ -25,6 +31,17 @@ class Ray {
 
         const den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
         if (den == 0) {
+            return;
+        }
+
+        const t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den;
+        const u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den;
+        if (t > 0 && t < 1 && u > 0) {
+            const pt = createVector();
+            pt.x = x1 + t * (x2 - x1);
+            pt.y = y1 + t * (y2 - y1);
+            return pt;
+        } else {
             return;
         }
     }
