e2d1f976272d2e8915de9d17109b1712c0068abe
Hoan Vu Van <congtunggv@gmail.com>
2021-07-19 21:54:27 +0700

    update agar

--- agario/sketch.js
+++ agario/sketch.js
@@ -1,11 +1,35 @@
 var blob;
 
+var blobs = [];
+var zoom = 1;
+
 function setup() {
     createCanvas(600,600);
-    blob = new Blob();
+    blob = new Agar(0, 0, 64);
+    for (let i = 0; i < 200; i++) {
+        var x = random(-width, width);
+        var y = random(-height, height);
+        blobs[i] = new Agar(x, y, 16);
+        
+    }
 }
 
 function draw() {
     background(0);
+
+    translate(width/2, height/2);
+    var newZoom = 64 / blob.r;
+    zoom = lerp(zoom, newZoom, 0.1)
+    scale(zoom);
+    translate(-blob.pos.x, -blob.pos.y)
     blob.show();
+    blob.update();
+
+    for (let i = blobs.length - 1; i >= 0; i--) {
+        blobs[i].show();
+        if (blob.eat(blobs[i])) {
+            blobs.splice(i, 1);
+        }
+        
+    }
 }
